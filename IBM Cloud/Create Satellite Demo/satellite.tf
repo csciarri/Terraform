bb63f8e319e378fa1c2f6deafae92e6c31ff808b
Chris Sciarrino <chris.sciarrino@gmail.com>
2022-02-13 21:00:57 -0500

    Update satellite.tf
    Attach hosts to Satellite

--- IBM Cloud/Create Satellite Demo/satellite.tf
+++ IBM Cloud/Create Satellite Demo/satellite.tf
@@ -2,10 +2,6 @@ resource "ibm_satellite_location" "sat" {
   location          = var.sat_location_name
   managed_from      = var.sat_managed_from
   resource_group_id = data.ibm_resource_group.rg.id
-
- provisioner "local-exec" {
-    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root -i private_ips.txt ~/Documents/GitHub/ansible/setupSatHosts.yaml"
-  }
 }
 
 data "ibm_satellite_attach_host_script" "script" {
@@ -14,3 +10,38 @@ data "ibm_satellite_attach_host_script" "script" {
   script_dir        = "/tmp"
   
 }
+
+resource "null_resource" "cluster" {
+   provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root -i private_ips.txt ~/Documents/GitHub/ansible/setupSatHosts.yaml"
+  }
+  depends_on = [data.ibm_satellite_attach_host_script.script]
+}
+
+
+data "ibm_satellite_location" "sat" {
+  location  = var.sat_location_name
+}
+
+output "name" {
+  value = data.ibm_satellite_location.sat.hosts
+}
+/*
+resource "ibm_satellite_host" "assign_master001" {
+  location      = ibm_satellite_location.sat.id
+  host_id       = "${lookup(data.ibm_satellite_location.sat.hosts[0], "host_id")}"
+  zone          = "${var.region}-1"
+}
+
+resource "ibm_satellite_host" "assign_master002" {
+  location      = ibm_satellite_location.sat.id
+  host_id       = "${lookup(data.ibm_satellite_location.sat.hosts[1], "host_id")}"
+  zone          = "${var.region}-2"
+}
+
+resource "ibm_satellite_host" "assign_master003" {
+  location      = ibm_satellite_location.sat.id
+  host_id       = "${lookup(data.ibm_satellite_location.sat.hosts[2], "host_id")}"
+  zone          = "${var.region}-3"
+}
+*/

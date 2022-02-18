1c55aa564772d9bdbf075db40bd525dcb2188d1b
Chris Sciarrino <chris.sciarrino@gmail.com>
2022-02-17 22:56:33 -0500

    Update variables.tf

--- IBM Cloud/modules/createVPC/variables.tf
+++ IBM Cloud/modules/createVPC/variables.tf
@@ -1,13 +1,11 @@
 variable "resource_group" {
   description = "Name of resource group to create VPC"
   type        = string
-  default     = "default"
 }
 
 variable "vpc_name" {
   description = "Name of VPC"
   type        = string
-  default     = "demo-vpc"
 }
 
 variable "ssh-key-name" {
@@ -21,27 +19,13 @@ variable "ssh-key" {
   type        = string
 }
 
-variable "vsi-image" {
-  description = "Name of the VSI images to use"
-  type        = string
-  default     = "ibm-redhat-7-9-minimal-amd64-4"
-}
-
-variable "vsi-profile" {
-  description = "Profile of the VSI to use"
-  type        = string
-  default     = "bx2-4x16"
-}
-
 variable "address_prefix_management" {
   description = "Use the default VPC address prefix or not"
   type        = string
-  default     = "manual"
 }
 
 variable "region" {
   description = "Where to create the resources"
-  default     = "us-south"
   type        = string
 }
 
